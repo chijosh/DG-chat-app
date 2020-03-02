c7b2efd2d810d588360fa20cdc3a93780a8c9a07
Chi Joshua <chijosh2010@hotmail.com>
2020-03-02 22:33:27 +0200

    fixed host link

--- client/src/components/views/ChatPage/ChatPage.js
+++ client/src/components/views/ChatPage/ChatPage.js
@@ -14,7 +14,7 @@ export class ChatPage extends Component {
   };
 
   componentDidMount() {
-    let server = 'http://localhost:5000';
+    let server = '/';
 
     this.props.dispatch(getChats());
 
